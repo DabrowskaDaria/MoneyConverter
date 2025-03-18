32d3055b389a33ffd4bb3e1fed40fcb2fe488e11
DabrowskaDaria <95188151+DabrowskaDaria@users.noreply.github.com>
2025-03-18 17:05:11 +0100

    Zadanie #5160 - adding a page displaying correct password change

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,38 +18,50 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends AbstractController
 {
-    #[Route('/register', name: 'registerUser')]
-    public function register(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, Token $token,EmailService $emailService,UrlGeneratorInterface $urlGenerator) : Response
+
+    public function __construct(
+        private UserRepository              $userRepository,
+        private EmailService                $emailService,
+        private UrlGeneratorInterface       $urlGenerator,
+        private UserPasswordHasherInterface $passwordHasher,
+        private TokenGeneratorInterface     $tokenGenerator
+    )
     {
-        $userDTO= new UserDTO();
-        $form=$this->createForm(RegisterForm::class, $userDTO);
+    }
+
+    #[Route('/register', name: 'registerUser')]
+    public function register(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, Token $token, EmailService $emailService, UrlGeneratorInterface $urlGenerator): Response
+    {
+        $userDTO = new UserDTO();
+        $form = $this->createForm(RegisterForm::class, $userDTO);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
 
-            $user= new User(
+            $user = new User(
                 $userDTO->getName(),
-                $userDTO ->getSurname(),
+                $userDTO->getSurname(),
                 $userDTO->getEmail(),
                 "",
                 "",
                 false
             );
-            $hashedPassword=$passwordHasher->hashPassword($user, $userDTO->getPassword());
+            $hashedPassword = $this->passwordHasher->hashPassword($user, $userDTO->getPassword());
             $user->setPassword($hashedPassword);
             $userRepository->save($user);
-            $token=$token->generateActivationToken($user,$userRepository);
-            $activationLink=$urlGenerator->generate(
+            $token = $token->generateActivationToken($user, $userRepository);
+            $activationLink = $urlGenerator->generate(
                 'activationLink',
-                ['token'=>$token],
+                ['token' => $token],
                 UrlGeneratorInterface::ABSOLUTE_URL
             );
-            $template="email/emailActiveAccount.html.twig";
+            $template = "email/emailActiveAccount.html.twig";
             $emailService->sendMail(
                 $user->getEmail(),
                 'Aktywacja konta',
@@ -61,22 +73,22 @@ class UserController extends AbstractController
             return $this->redirectToRoute('registerUser');
         }
 
-        return $this->render('User/register.html.twig',[
-            'form'=>$form->createView(),
+        return $this->render('User/register.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 
     #[Route('/activateAccount/{token}', name: 'activationLink')]
-    public function activateAccount(string $token,UserRepository $userRepository) : Response
+    public function activateAccount(string $token, UserRepository $userRepository): Response
     {
-        $user=$userRepository->findOneByToken($token);
-        if($user->getTokenExpiresAt()>new \DateTime() && $token==$user->getToken()){
+        $user = $userRepository->findOneByToken($token);
+        if ($user->getTokenExpiresAt() > new \DateTime() && $token == $user->getToken()) {
             $user->setToken('');
             $user->setTokenExpiresAt(null);
             $user->setActive(true);
             $userRepository->save($user);
             return new Response('Aktywowano konto');
-        }else{
+        } else {
             $user->setToken('');
             $user->setTokenExpiresAt(null);
             $userRepository->save($user);
@@ -84,81 +96,81 @@ class UserController extends AbstractController
         }
     }
 
-    #[Route('/remindPassword', name: 'remindPassword',methods: ['GET'])]
-    public function remindPasswordForm() : Response
+    #[Route('/remindPassword', name: 'remindPassword', methods: ['GET'])]
+    public function remindPasswordForm(): Response
     {
         return $this->render('User/remindPassword.html.twig');
     }
 
-    #[Route('/remindPassword',name: 'remindPassword_post', methods: ['POST'])]
-    public function remindPassword(Request $request,UserRepository $userRepository, EmailService $emailService, Token $token,UrlGeneratorInterface $urlGenerator) : Response
+    #[Route('/remindPassword', name: 'remindPassword_post', methods: ['POST'])]
+    public function remindPassword(Request $request, UserRepository $userRepository, EmailService $emailService, Token $token, UrlGeneratorInterface $urlGenerator): Response
     {
-        $email=$request->request->get('email');
-        $user=$userRepository->findOneByEmail($email);
-        if(!$user){
-            $this->addFlash('info','Nie znaleziono użytkownika z takim adresem e-mail.');
+        $email = $request->request->get('email');
+        $user = $userRepository->findOneByEmail($email);
+        if (!$user) {
+            $this->addFlash('info', 'Nie znaleziono użytkownika z takim adresem e-mail.');
             return $this->redirectToRoute('remindPassword');
         }
-        $token=$token->generateToken($user,$userRepository);
-        $resetLink=$urlGenerator->generate('resetPassword',['token'=>$token],UrlGeneratorInterface::ABSOLUTE_URL);
+        $token = $token->generateToken($user, $userRepository);
+        $resetLink = $urlGenerator->generate(
+            'resetPassword',
+            ['token' => $token],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
 
-        //$content=sprintf("<p> Witaj!!<br> Kliknij w link, aby zresetować hasło: </p><a href='%s' >Resetuj hasło</a>",$resetLink);
-
-        $emailService->sendMail($user->getEmail(),'Resetowanie hasła',$user->getName(), $user->getSurname(),$resetLink,'email/emailResetPassword.html.twig');
+        $emailService->sendMail($user->getEmail(), 'Resetowanie hasła', $user->getName(), $user->getSurname(), $resetLink, 'email/emailResetPassword.html.twig');
 
         $this->addFlash('success', 'E-mail z instrukcjami resetowania hasła został wysłany.');
         return $this->redirectToRoute('remindPassword');
     }
 
-
-//    #[Route('/resetPassword', name:'resetPassword', methods: ['GET'])]
-//    public function resetPasswordForm() : Response
-//    {
-//        return $this->render('User/resetPassword.html.twig');
-//    }
-
-    #[Route('/resetPassword', name: 'resetPassword')]
-    public function resetPassword(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher):Response
+    #[Route(path: '/resetPassword/success', name: 'reset_password_success', methods: ['GET'])]
+    public function showRestPasswordSuccess(): Response
     {
-        $token=$request->query->get('token');
-        $user=$userRepository->findOneByToken($token);
+        return $this->render('User/restPasswordSuccess.html.twig');
+    }
 
-        if(!$user)
-        {
+    #[Route('/resetPassword/{token}', name: 'resetPassword',methods: ['GET','POST'])]
+    public function resetPassword(string $token, Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = $userRepository->findOneByToken($token);
+
+        if (!$user) {
             $this->addFlash('error', 'Token jest niepoprawny');
-            return $this->redirectToRoute('resetPassword');
+            return $this->redirectToRoute('resetPassword', ['token' => $token]);
         }
 
-        if($user->getTokenExpiresAt()< new \DateTime()){
+        if ($user->getTokenExpiresAt() < new \DateTime()) {
             $this->addFlash('error', 'Token wygasł');
-            return $this->redirectToRoute('resetPassword');
+            return $this->redirectToRoute('resetPassword', ['token' => $token]);
         }
 
-
-        $passwordDTO= new PasswordDTO();
-        $form=$this->createForm(ResetPasswordForm::class, $passwordDTO);
+        $passwordDTO = new PasswordDTO();
+        $form = $this->createForm(ResetPasswordForm::class, $passwordDTO);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
-            $passwordData=$form->getData();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $passwordData = $form->getData();
 
-            $password=$passwordData->getPassword();
-            $repeatPassword=$passwordData->getRepeatPassword();
+            $password = $passwordData->getPassword();
+            $repeatPassword = $passwordData->getRepeatPassword();
 
-            if($password!==$repeatPassword){
+            if ($password !== $repeatPassword) {
                 $this->addFlash('error', "Wprowadź identyczne hasła.");
-                return $this->redirectToRoute('resetPassword');
+                return $this->redirectToRoute('resetPassword', ['token' => $token]);
             }
 
-            $user->setPassword($passwordHasher->hashPassword($user,$password));
+            $user->setPassword($passwordHasher->hashPassword($user, $password));
             $user->setToken('');
             $user->setTokenExpiresAt(null);
             $userRepository->save($user);
             $this->addFlash('success', "Hasło zostało zmienione.");
-            return $this->redirectToRoute('resetPassword');
+
+            return $this->redirectToRoute('reset_password_success');
         }
-        return $this->render('User/resetPassword.html.twig',[
-            'form'=>$form->createView(),
+
+        return $this->render('User/resetPassword.html.twig', [
+            'form' => $form->createView(),
         ]);
 
     }
